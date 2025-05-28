6b446598c4af3bc566aadeef3e679eda94f158c6
Matthew Okadinya <matthewokadinya@gmail.com>
2025-05-28 08:39:20 +0100

    Implement SignupForm component with email and password fields

--- src/app/(auth)/signup/component/SignupForm.tsx
+++ src/app/(auth)/signup/component/SignupForm.tsx
@@ -1,24 +1,16 @@
+// SignupForm.tsx
+"use client";
 
+export default function SignupForm() {
+  return (
+    <form>
+      <label>Email</label>
+      <input name="userEmail" type="email" />
 
+      <label>Password</label>
+      <input name="userPassword" type="password" />
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+      <button type="submit">Sign up</button>
+    </form>
+  );
+}
