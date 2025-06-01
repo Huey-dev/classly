923ced2dac5eabb2f0265eddf4c0618788c1c308
Matthew Okadinya <matthewokadinya@gmail.com>
2025-06-01 19:06:21 +0100

    Add imports and initialize PrismaClient in login route

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -1,1 +1,6 @@
-import { NextResponse,  } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+import bcrypt from "bcryptjs"
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+// when i want to log my users in, what do i do
