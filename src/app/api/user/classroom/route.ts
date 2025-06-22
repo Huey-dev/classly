8cbdef1edf435fb881850ac7b79c24103c2caba5
Matthew Okadinya <matthewokadinya@gmail.com>
2025-06-22 16:23:45 +0100

    Implement initial POST request handler in classroom API route

---
+++ src/app/api/user/classroom/route.ts
@@ -0,0 +1,9 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request: NextRequest) {
+    try {
+        // add logic
+    } catch (error) {
+        // add error statements
+    }
+}
