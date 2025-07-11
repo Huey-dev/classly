b1b479e0c3faca4a520f0565e6266d9f59b84250
Matthew Okadinya <matthewokadinya@gmail.com>
2025-07-11 05:40:05 +0100

    feat: implement authentication middleware and update NextAuth configuration

--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,33 @@
+import NextAuth from "next-auth";
+import { authConfig } from "./auth.config";
 import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
-import jwt from "jsonwebtoken";
+
+const privateRoutes = ["/dashboard"];
+
+const { auth } = NextAuth(authConfig);
+export default auth(async (req) => {
+  const isLoggedIn = !!req.auth;
+  const { nextUrl } = req;
+  const isLoginRoute = nextUrl.pathname.startsWith("/signin");
+
+  if (isLoggedIn && isLoginRoute) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+  if (
+    !isLoggedIn &&
+    privateRoutes.some((route) => nextUrl.pathname.startsWith(route))
+  ) {
+    return NextResponse.redirect(new URL("/onboarding", req.url));
+  }
+});
+
+export const config = {
+  matcher: [
+    "/((?!api|_next/static|_next/image|favicon.ico).*)",
+    "/dashboard/:path*",
+    "/signin",
+    "/signup",
+    "/onboarding",
+    "/classroom/:path*",
+  ],
+};
--- src/app/api/user/classroom/route.ts
+++ src/app/api/user/classroom/route.ts
@@ -1,7 +1,15 @@
 import { NextRequest, NextResponse } from "next/server";
-
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
 export async function POST(request: NextRequest) {
     try {
+        const body = await request.json()
+        // const newClass= await prisma.user.create({
+        //     data: {
+        //         // email,
+        //         // password: hashedPassword,
+        //     },
+        // });
         // add logic
     } catch (error) {
         // add error statements
